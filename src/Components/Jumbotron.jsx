3b034bed21d43f27b056bc677c3b6fa9b761c9bc
Hambasahaya <zaldyramlan24@gmail.com>
2025-05-10 16:04:44 +0700

    beta1

--- src/Components/Jumbotron.jsx
+++ src/Components/Jumbotron.jsx
@@ -7,7 +7,7 @@ export default function Jumbotron(){
             <div className="col d-flex flex-column">
                 <p className="mt-2">Welcome to The Daira Room Apartemen</p>
                 <h4>Find Your Depok Apartment Fast, Right from Your Phone</h4>
-                <p>Temukan Apartemen Impian Anda di Depok – Cepat, Mudah, Terpercaya</p>
+                <p>Temukan Apartemen Impian Anda di Depok – Cepat, Mudah, Terpercaya Start from 80k!!</p>
             </div>
             <div className="col d-flex flex-column ">
                 <img src="/img/apart.png" alt="" srcset="" />
